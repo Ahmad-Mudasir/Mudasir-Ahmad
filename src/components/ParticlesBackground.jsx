009a8fda2438c49ddde186ca7a67764c79a59aef
Mudasir Ahmad <106972160+nawaz311412@users.noreply.github.com>
2024-09-05 12:59:45 +0500

    i added particles

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -1,81 +1,83 @@
-
 import Particles from "react-tsparticles";
-
+import { loadSlim } from "tsparticles-slim";
 
 // eslint-disable-next-line react/prop-types
-function ParticlesBackground({ children }) {
+const ParticlesBackground = ({ children }) => {
+  const particlesInit = async (main) => {
+    await loadSlim(main);
+  };
 
-   
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
-    return (
-        <div id="particles-js">
-            <Particles
-                id="tsparticles"
-               
-                options={{
-                    fpsLimit: 120,
-                    interactivity: {
+  return (
+    <div id="particles-js">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ec6e59",
+            },
+            links: {
+              color: "#ec6e59",
+              distance: 150,
+              enable: true,
+              opacity: 0.3,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: true,
+              speed: 2,
+              straight: true,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 900,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 3 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
 
-                        modes: {
-                            push: {
-                                quantity: 4
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4
-                            }
-                        }
-                    },
-                    particles: {
-                        color: {
-                           
-                            value: "#ec6e59",
-                            
-                        },
-                        links: {
-                           
-                            color: "#ec6e59",
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.3,
-                            width: 1
-                        },
-                        collisions: {
-                            enable: true
-                        },
-                        move: {
-                            direction: "none",
-                            enable: true,
-                            outModes: {
-                                default: "bounce"
-                            },
-                            random: true,
-                            speed: 2,
-                            straight: true
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                area: 900
-                            },
-                            value: 80
-                        },
-                        opacity: {
-                            value: 0.5
-                        },
-                        shape: {
-                            type: "circle"
-                        },
-                        size: {
-                            value: { min: 1, max: 3 }
-                        }
-                    },
-                    detectRetina: true
-                }}
-            />
-            {children}
-        </div >
-    )
-}
+      {children}
+    </div>
+  );
+};
 
-export default ParticlesBackground
+export default ParticlesBackground;
